9d6f56d31e68e28b1cafb4352e800a60dd7d31de
Naphier Awalie <naphiera@gmail.com>
2026-06-08 13:19:34 +0800

    Update page.tsx

--- src/app/tech-stack/page.tsx
+++ src/app/tech-stack/page.tsx
@@ -3,7 +3,8 @@ import { TechStackClient } from "@/app/tech-stack/TechStackClient";
 
 export const metadata: Metadata = {
   title: "Tech Stack | Naphier Awalie",
-  description: "Explore the comprehensive tech stack, frameworks, tools, and platforms utilized by Naphier Awalie.",
+  description:
+    "Explore the comprehensive tech stack, frameworks, tools, and platforms utilized by Naphier Awalie.",
 };
 
 export default function TechStackPage() {

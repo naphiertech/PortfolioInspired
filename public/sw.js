988ff310f867666f948db3a8172e2554420cbc50
Naphier Awalie <naphiera@gmail.com>
2026-06-14 12:02:17 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v2";
+const CACHE_VERSION = "v3";
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `dynamic-${CACHE_VERSION}`;
 
@@ -19,6 +19,7 @@ const APP_SHELL = [
   "/certificates/BuildWithAIZampen.jpg",
   "/certificates/DICT-NotebookLM.png",
   "/certificates/GoogleIOExtended.jpg",
+  "/certificates/BuildWithAI_2026.png",
 ];
 
 // Install Event - Pre-cache the App Shell
